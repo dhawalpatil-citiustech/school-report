f1a9ce0cd118ae40a9086a62e858b94757e593b2
dhawalpatil-citiustech <dhawal.patil@citiustech.com>
2021-06-07 16:54:13 +0530

    Adding primeNg and Bootstrap for charts and styling

--- src/app/components/grade-piechart/grade-piechart.component.ts
+++ src/app/components/grade-piechart/grade-piechart.component.ts
@@ -7,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GradePiechartComponent implements OnInit {
 
+  data: any;
+
   constructor() {
+
   }
 
   ngOnInit(): void {
+    this.data = {
+      labels: ['Grade 1', 'Grade 2', 'Grade 3'],
+      datasets: [
+        {
+          data: [4, 6, 2],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }]
+    };
+  }
+
+  selectData(e: any) {
+    console.log(e.dataset);
+    console.log(e.element);
+    console.log(e.element._datasetIndex);
+    console.log(e.element._index);
   }
 
 }
